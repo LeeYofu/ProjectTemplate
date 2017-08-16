b798e5a95e7b4e58089e02857e78b65ed6bdbfcf
李友富 <leeyofu@foxmail.com>
2017-08-16 17:11:26 +0800

    2

--- ProjectTemplate/Class/AppDelegate/AppDelegate.m
+++ ProjectTemplate/Class/AppDelegate/AppDelegate.m
@@ -8,7 +8,6 @@
 
 #import "AppDelegate.h"
 #import "AppDelegate+AppService.h"
-#import "CustomTabBarController.h"
 
 @interface AppDelegate ()
 
@@ -18,7 +17,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
